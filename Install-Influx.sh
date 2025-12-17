59aa380c9392f7e7c3a5688a1e3d09b39a81815b
x86 <meap09121@gmail.com>
2025-12-17 11:23:41 -0800

    Added Influx Installer

---
+++ Install-Influx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Ubuntu and Debian
+# Add the InfluxData key to verify downloads and add the repository
+curl --silent --location -O https://repos.influxdata.com/influxdata-archive.key
+gpg --show-keys --with-fingerprint --with-colons ./influxdata-archive.key 2>&1 \
+| grep -q '^fpr:\+24C975CBA61A024EE1B631787C3D57159FC2F927:$' \
+&& cat influxdata-archive.key \
+| gpg --dearmor \
+| sudo tee /etc/apt/keyrings/influxdata-archive.gpg > /dev/null \
+&& echo 'deb [signed-by=/etc/apt/keyrings/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' \
+| sudo tee /etc/apt/sources.list.d/influxdata.list
+# Install influxdb
+sudo apt-get update -y && sudo apt-get install influxdb2 -y
+
+sudo service influxdb start
+
+#influxd --reporting-disabled
