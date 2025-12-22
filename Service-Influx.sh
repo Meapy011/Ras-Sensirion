288458c316382ff058001248a78c199cb78a1d5a
x86 <meap09121@gmail.com>
2025-12-22 14:10:58 -0800

    Added Exambles

--- Service-Influx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -r sensors-db ~/
-sudo cp -r readings-db.service /etc/systemd/system/
-sudo systemctl daemon-reexec
-sudo systemctl daemon-reload
-sudo systemctl enable readings-db.service
-sudo systemctl start readings-db.service
